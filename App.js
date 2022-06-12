11eabedfbd9140fdc90c9f67b1b001fc76bea9e0
Stas Ivanovskii <ivanovskiy999@gmail.com>
2022-06-12 17:55:04 +1000

    Change to stateful screens

--- App.js
+++ App.js
@@ -3,23 +3,30 @@ import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button title="New alarm" onPress={() => navigation.navigate('Alarm')} />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button
+          title="New alarm"
+          onPress={() => this.props.navigation.navigate('Alarm')}
+        />
+      </View>
+    );
+  }
 }
 
-function AlarmScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        title="OK"
-        onPress={() => navigation.navigate('Awesome Radio Alarm')}
-      />
-    </View>
-  );
+class AlarmScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button
+          title="OK"
+          onPress={() => this.props.navigation.navigate('Awesome Radio Alarm')}
+        />
+      </View>
+    );
+  }
 }
 
 const Stack = createNativeStackNavigator();
